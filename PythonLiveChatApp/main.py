545b9b7f1022b2ab37d7b997f4f43dd86288552f
evilusean <shawntemesy@hotmail.com>
2023-03-06 16:24:57 -0400

    update

--- PythonLiveChatApp/main.py
+++ PythonLiveChatApp/main.py
@@ -1,9 +1,16 @@
-#socket server: uses sockets to communicate without refreshing the page, transmits to all clients listening for the updated messages
-from flask import flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+#import modules
+#socket server: uses sockets to communicate without refreshing the page,
+# transmits to all clients listening for the updated messages
 
+
+#basic setup for any flask webserver/application
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "abcde12345"
 socketio = SocketIO(app)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
