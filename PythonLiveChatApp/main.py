b7daf7a1b7cfdc495ba9ea4a858d180f7e7512fe
evilusean <shawntemesy@hotmail.com>
2023-03-08 09:43:14 -0400

    Added Room generation and get methods for forms and error messages

--- PythonLiveChatApp/main.py
+++ PythonLiveChatApp/main.py
@@ -9,10 +9,35 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "abcde12345"
 socketio = SocketIO(app)
 
+rooms = {}
+def generate_unique_code(Length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+
+    return code
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please Enter a Name.")
+        
+        if join != False and not code:
+            return render_template("home.html", error="Please Enter a Room Code.")
+        
+        room = code 
+        if create != False:
+            room = generate_unique_code(4)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
