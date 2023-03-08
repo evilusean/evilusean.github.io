a444d8401cf6c8873bf21d9a9654ad909baad0e0
evilusean <shawntemesy@hotmail.com>
2023-03-08 04:20:00 -0400

    removed comments to debug errors

--- PythonLiveChatApp/main.py
+++ PythonLiveChatApp/main.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, session, redirect
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
-#import modules
-#socket server: uses sockets to communicate without refreshing the page,
-# transmits to all clients listening for the updated messages
 
 
 #basic setup for any flask webserver/application
@@ -12,8 +9,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "abcde12345"
 socketio = SocketIO(app)
 
-#set up a route with decorater syntax "@app" to the home page with methods, posting and getting 
-#renders a template file for home.html
+
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("home.html")
