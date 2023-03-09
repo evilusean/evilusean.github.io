1a82ea7a165ef960196a16d2246fb0aa78f59cb9
evilusean <shawntemesy@hotmail.com>
2023-03-09 13:36:04 -0400

    finished home.html function and added room.html function

--- PythonLiveChatApp/main.py
+++ PythonLiveChatApp/main.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
-
-#basic setup for any flask webserver/application
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "abcde12345"
 socketio = SocketIO(app)
 
 rooms = {}
+
 def generate_unique_code(Length):
     while True:
         code = ""
@@ -23,6 +22,7 @@ def generate_unique_code(Length):
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    session.clear()
     if request.method == "POST":
         name = request.form.get("name")
         code = request.form.get("code")
@@ -30,14 +30,27 @@ def home():
         create = request.form.get("create", False)
 
         if not name:
-            return render_template("home.html", error="Please Enter a Name.")
+            return render_template("home.html", error="Please Enter a Name." code=code, name=name)
         
         if join != False and not code:
-            return render_template("home.html", error="Please Enter a Room Code.")
+            return render_template("home.html", error="Please Enter a Room Code." code=code, name=name)
         
         room = code 
         if create != False:
             room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist." code=code, name=name)
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+
+    return render_template("home.html")
+
+@app.route("/room")
+def room():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
