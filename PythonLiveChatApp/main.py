42cefeee88822f0059727128202790aec36b49bd
evilusean <shawntemesy@hotmail.com>
2023-03-09 19:53:45 -0400

    added save messages on refresh, created message for loop
    added final styling

--- PythonLiveChatApp/main.py
+++ PythonLiveChatApp/main.py
@@ -54,7 +54,7 @@ def room():
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
 
-    return render_template("room.html", code=room)
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
 
 @socketio.on("message")
 def message(data):
