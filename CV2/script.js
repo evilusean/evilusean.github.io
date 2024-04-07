b9847a6c477dd2e2cd9e3e102bb689890e298f58
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-07 13:27:41 -0300

    add scripts file
    add scripts file

---
+++ CV2/script.js
@@ -0,0 +1,5 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = docutment.querySelector("#navbar");
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
