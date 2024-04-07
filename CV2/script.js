35af6f2011687b349da538d644dad5b5ff01c1f2
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-07 17:18:13 -0300

    Update script.js
    fixed typo

--- CV2/script.js
+++ CV2/script.js
@@ -14,7 +14,7 @@ window.onscroll = () => {
       navLinks.forEach((links) => {
         links.classList.remove("active");
         document
-          .querySelector("header nav a href*=" + id + " ]")
+          .querySelector("header nav a [href*=" + id + " ]")
           .classList.add("active");
       });
     }
