a0f5bd681685fd5d307c834089a3148cc718aecb
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-08 23:06:23 -0300

    Update script.js
    moto

--- MatrixRain/script.js
+++ MatrixRain/script.js
@@ -14,7 +14,18 @@ class Symbol {
     this.text = "";
     this.canvasHeight = canvasHeight;
   }
-  draw() {}
+  draw() {
+    this.text = this.characters.charAt(
+      Math.floor(Math.random() * this.characters.length)
+    );
+    context.fillStyle = "#0aff0a";
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight) {
+      this.y = 0;
+    } else {
+      this.y += 1;
+    }
+  }
 }
 
 class Effect {
@@ -25,13 +36,24 @@ class Effect {
     this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
     this.#initialize();
+    console.log(this.symbols);
   }
 
   #initialize() {
-    for (let i = 0; i < this.columns.length; i++) {
-      this.symbols[i] = new Symbol();
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
   }
 }
 
-function animate() {}
+const effect = new Effect(canvas.width, canvas.height);
+
+function animate() {
+  ctx.fillStyle = "rgba(0,0,0,0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = effect.fontSize + "px monospace";
+  effect.symbols.forEach((symbol) => symbol.draw(ctx));
+  requestAnimationFrame(animate);
+}
+
+animate();
