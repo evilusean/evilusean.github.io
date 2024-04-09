5a282b3b33c91d270dfa3472b3e58a31b6e44942
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-08 21:32:26 -0300

    Update script.js
    progressSean

--- MatrixRain/script.js
+++ MatrixRain/script.js
@@ -1,2 +1,37 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters =
+      "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.text = "";
+    this.canvasHeight = canvasHeight;
+  }
+  draw() {}
+}
+
+class Effect {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 25;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+  }
+
+  #initialize() {
+    for (let i = 0; i < this.columns.length; i++) {
+      this.symbols[i] = new Symbol();
+    }
+  }
+}
+
+function animate() {}
