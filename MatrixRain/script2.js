4912ff2862ce9c8447b34bf79a7fce6c0afc70a2
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-09 00:56:36 -0300

    ONEGAI
    test kind of sort of worked

---
+++ MatrixRain/script2.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight, text) {
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.text = text;
+    this.canvasHeight = canvasHeight;
+  }
+  draw(context) {
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.95) {
+      this.y = 0;
+    } else {
+      this.y += 1;
+    }
+  }
+}
+
+class Effect {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 25;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.text = "ONEGAI PLEASE HIRE ME";
+    this.textIndex = 0;
+    this.#initialize();
+  }
+  #initialize() {
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight, "");
+    }
+  }
+  resize() {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+  }
+  updateText() {
+    this.symbols.forEach((symbol, index) => {
+      if (this.textIndex >= this.text.length) {
+        this.textIndex = 0;
+      }
+      symbol.text = this.text[this.textIndex];
+      this.textIndex++;
+    });
+  }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 10; //Change this to speed up or slow down
+const nextFrame = 1000 / fps;
+let timer = 0;
+
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.textAlign = "center";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#0aff0a";
+    ctx.font = effect.fontSize + "px monospace";
+    effect.updateText();
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+  requestAnimationFrame(animate);
+}
+animate(0);
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height);
+});
