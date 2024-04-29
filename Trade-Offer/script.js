d0bbff79fb565fc5a9516b1a0106424bc00e63ce
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-29 01:09:26 -0300

    updated text
    updated text

--- Trade-Offer/script.js
+++ Trade-Offer/script.js
@@ -1,9 +1,12 @@
+//selects divs
 const giveDiv = document.querySelector(".give");
 const takeDiv = document.querySelector(".take");
 
+//gets url params
 const urlParams = new URLSearchParams(window.location.search);
 const giveValue = urlParams.get("Give");
 const takeValue = urlParams.get("Take");
 
+//updates div values on index.html
 giveDiv.textContent = giveValue;
 takeDiv.textContent = takeValue;
