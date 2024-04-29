8f0e86c3c2ca95c665481ed324ab5613c4dde80b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-29 00:20:02 -0300

    Update script.js
    first attempt, going to change

---
+++ Trade-Offer/script.js
@@ -0,0 +1,22 @@
+const giveInput = document.querySelector(".give-input");
+const takeInput = document.querySelector(".take-input");
+const giveDiv = document.querySelector(".give");
+const takeDiv = document.querySelector(".take");
+
+giveInput.addEventListener("input", () => {
+  giveDiv.textContent = giveInput.value;
+  giveDiv.style.fontSize = `${100 - giveInput.value.length / 2}%`;
+});
+
+takeInput.addEventListener("input", () => {
+  takeDiv.textContent = takeInput.value;
+  takeDiv.style.fontSize = `${100 - takeInput.value.length / 2}%`;
+});
+
+const submitButton = document.querySelector("button");
+submitButton.addEventListener("click", () => {
+  const giveValue = giveInput.value;
+  const takeValue = takeInput.value;
+  const url = `evilusean.github.io/trade-offer/Give=${giveValue}&Take=${takeValue}`;
+  window.location.href = url;
+});
