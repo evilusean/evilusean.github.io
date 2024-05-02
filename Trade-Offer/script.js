ea50ab3590b6f08121cd594527a6a45ff31c3d3b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-02 08:40:46 -0300

    Update script.js
    added URL decoder, '/%0A/g' looks weird to me, might need to change, should be '%0A', why the '/'?

--- Trade-Offer/script.js
+++ Trade-Offer/script.js
@@ -4,9 +4,26 @@ const takeDiv = document.querySelector(".takeDiv");
 
 //gets url params
 const urlParams = new URLSearchParams(window.location.search);
-const giveValue = urlParams.get("Give");
-const takeValue = urlParams.get("Take");
+const giveURL = urlParams.get("Give");
+const takeURL = urlParams.get("Take");
+
+const giveValue = decodeURLParameter(urlParams.get("Give"));
+const takeValue = decodeURLParameter(urlParams.get("Take"));
 
 //updates div values on index.html
 giveDiv.textContent = giveValue;
 takeDiv.textContent = takeValue;
+
+function decodeURLParameter(param) {
+  // Decode the URL parameter
+  const decodedParam = decodeURIComponent(param);
+
+  // Replace all %20 with spaces
+  const withSpaces = decodedParam.replace(/%20/g, " ");
+
+  // Replace all %0A with newlines
+  const withNewlines = withSpaces.replace(/%0A/g, "\n");
+
+  // Return the decoded and formatted parameter
+  return withNewlines;
+}
