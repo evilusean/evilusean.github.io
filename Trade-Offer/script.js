d6ce2c25d920c564550cf9ed54279058da9ac87a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-02 19:50:50 -0300

    reverted to decoder, added 5 give text inputs
    added new text input, going to change it so it automatically adds each to a new line, encoder will have to just do spaces

--- Trade-Offer/script.js
+++ Trade-Offer/script.js
@@ -7,17 +7,23 @@ const urlParams = new URLSearchParams(window.location.search);
 const giveURL = urlParams.get("Give"); //new function should work without these 2
 const takeURL = urlParams.get("Take"); // maybe remove, test first
 
-//const giveValue = decodeURLParameter(urlParams.get("Give"));
-//const takeValue = decodeURLParameter(urlParams.get("Take"));
-
-// Decode the URL parameters
-const giveValue =
-  decodeURI(giveURL) ||
-  "Certificate of Eligibility (COE) <br> $ for Flight <br> $ for Rent <br> Salary";
-const takeValue =
-  decodeURI(takeURL) ||
-  "Jr. Web Developer <br> Jr. Data Analyst <br> Jr. Software Engineer <br> IT Specialist <br> Native English Speaker ";
+const giveValue = decodeURLParameter(urlParams.get("Give"));
+const takeValue = decodeURLParameter(urlParams.get("Take"));
 
 //updates div values on index.html
 giveDiv.textContent = giveValue;
 takeDiv.textContent = takeValue;
+
+function decodeURLParameter(param) {
+  // Decode the URL parameter
+  const decodedParam = decodeURIComponent(param);
+
+  // Replace all %20 with spaces
+  const withSpaces = decodedParam.replace(/%20/g, " ");
+
+  // Replace all %0A with newlines
+  const withNewlines = withSpaces.replace(/%0A/g, "\n");
+
+  // Return the decoded and formatted parameter
+  return withNewlines;
+}
