f24ebccda516b29aec94014cd0c7d5c33bc713c9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-02 08:58:29 -0300

    decodeURI()
    trying new built in decodeURI() method

--- Trade-Offer/script.js
+++ Trade-Offer/script.js
@@ -7,23 +7,13 @@ const urlParams = new URLSearchParams(window.location.search);
 const giveURL = urlParams.get("Give"); //new function should work without these 2
 const takeURL = urlParams.get("Take"); // maybe remove, test first
 
-const giveValue = decodeURLParameter(urlParams.get("Give"));
-const takeValue = decodeURLParameter(urlParams.get("Take"));
+//const giveValue = decodeURLParameter(urlParams.get("Give"));
+//const takeValue = decodeURLParameter(urlParams.get("Take"));
+
+// Decode the URL parameters
+const giveValue = decodeURI(giveURL);
+const takeValue = decodeURI(takeURL);
 
 //updates div values on index.html
 giveDiv.textContent = giveValue;
 takeDiv.textContent = takeValue;
-
-function decodeURLParameter(param) {
-  // Decode the URL parameter
-  const decodedParam = decodeURIComponent(param);
-
-  // Replace all %20 with spaces
-  const withSpaces = decodedParam.replace(/%20/g, " ");
-
-  // Replace all %0A with newlines
-  const withNewlines = withSpaces.replace(/%0A/g, "\n");
-
-  // Return the decoded and formatted parameter
-  return withNewlines;
-}
