b9a185781aa70dfba87c5ea0726929443d539ebd
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-03 05:49:54 -0300

    added default values for test
    this should give me default values,  so I can send the link with just 'https://evilusean.github.io/Trade-Offer/'

--- Trade-Offer/script.js
+++ Trade-Offer/script.js
@@ -1,16 +1,16 @@
 //gets url params
 const urlParams = new URLSearchParams(window.location.search);
 //selects URL Variables
-const give1 = urlParams.get("G1");
-const give2 = urlParams.get("G2");
-const give3 = urlParams.get("G3");
-const give4 = urlParams.get("G4");
-const give5 = urlParams.get("G5");
-const take1 = urlParams.get("T1");
-const take2 = urlParams.get("T2");
-const take3 = urlParams.get("T3");
-const take4 = urlParams.get("T4");
-const take5 = urlParams.get("T5");
+const give1 = urlParams.get("G1") || "Give 1";
+const give2 = urlParams.get("G2") || "Give 2";
+const give3 = urlParams.get("G3") || "Give 3";
+const give4 = urlParams.get("G4") || "Give 4";
+const give5 = urlParams.get("G5") || "Give 5";
+const take1 = urlParams.get("T1") || "Take 1";
+const take2 = urlParams.get("T2") || "Take 2";
+const take3 = urlParams.get("T3") || "Take 3";
+const take4 = urlParams.get("T4") || "Take 4";
+const take5 = urlParams.get("T5") || "Take 5";
 
 //Updates our <h2> tags with URL variables
 document.querySelector(".give-1").textContent = give1;
