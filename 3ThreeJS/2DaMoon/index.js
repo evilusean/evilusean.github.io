aec525e514262c937a344696ae2fcf3ab4819803
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-27 13:31:17 -0300

    added rest
    just copying it all - remove/change what I don't need, going to have to rename everything anyways
    change all variable names - getting all the needed pieces together first

--- 3ThreeJS/2DaMoon/index.js
+++ 3ThreeJS/2DaMoon/index.js
@@ -33,3 +33,55 @@ const material = new THREE.MeshPhongMaterial({
 // material.map.colorSpace = THREE.SRGBColorSpace;
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
+
+const lightsMat = new THREE.MeshBasicMaterial({
+    map: loader.load("./textures/03_earthlights1k.jpg"),
+    blending: THREE.AdditiveBlending,
+  });
+  const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+  earthGroup.add(lightsMesh);
+  
+  const cloudsMat = new THREE.MeshStandardMaterial({
+    map: loader.load("./textures/04_earthcloudmap.jpg"),
+    transparent: true,
+    opacity: 0.8,
+    blending: THREE.AdditiveBlending,
+    alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'),
+    // alphaTest: 0.3,
+  });
+  const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+  cloudsMesh.scale.setScalar(1.003);
+  earthGroup.add(cloudsMesh);
+  
+  const fresnelMat = getFresnelMat();
+  const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+  glowMesh.scale.setScalar(1.01);
+  earthGroup.add(glowMesh);
+  
+  const stars = getStarfield({numStars: 2000});
+  scene.add(stars);
+  
+  const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
+  sunLight.position.set(-2, 0.5, 1.5);
+  scene.add(sunLight);
+  
+  function animate() {
+    requestAnimationFrame(animate);
+  
+    earthMesh.rotation.y += 0.002; //Starts the earth rotation
+    lightsMesh.rotation.y += 0.002;
+    cloudsMesh.rotation.y += 0.0023;
+    glowMesh.rotation.y += 0.002;
+    stars.rotation.y -= 0.0002;
+    renderer.render(scene, camera);
+  }
+  
+  animate();
+  
+  function handleWindowResize () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener('resize', handleWindowResize, false);
+  
