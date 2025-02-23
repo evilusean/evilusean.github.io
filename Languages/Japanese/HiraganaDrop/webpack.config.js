7c8221f884d102ecce4054de4701a3c9db6116da
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-23 05:23:22 -0400

    Update webpack.config.js
    favicon

--- Languages/Japanese/HiraganaDrop/webpack.config.js
+++ Languages/Japanese/HiraganaDrop/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -24,6 +25,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
       filename: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' }
+      ]
     })
   ],
   devServer: {
