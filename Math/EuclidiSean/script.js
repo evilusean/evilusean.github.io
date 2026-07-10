6597f976715615f3ce2e53e9c96265f6d51d28de
Sean Teams <shawntemesy@hotmail.com>
2026-07-09 21:58:51 -0300

    Create script.js

---
+++ Math/EuclidiSean/script.js
@@ -0,0 +1,4 @@
+const euclidData = { /* To be populated by Cursor prompt */ };
+document.getElementById('randomize').addEventListener('click', () => {
+    // Logic to select random proposition/step
+});
